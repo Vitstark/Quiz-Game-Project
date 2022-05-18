2c9283ebb2b090ca356d5d084753e1e892664ae1
Vitalii Chekushkin <89994306+Vitstark@users.noreply.github.com>
2022-05-18 16:57:38 +0300

    изменена конфигурация xml

---
+++ src/main/java/ru/quizgameproject/WebController.java
@@ -0,0 +1,18 @@
+package ru.quizgameproject;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebController {
+    @GetMapping("/login")
+    public String getLoginPage() {
+        return "login";
+    }
+
+    @GetMapping("/mainpage")
+    public String getMainPage() {
+        return "mainpage";
+    }
+
+}
